01ff92ff0515c0a55b8cfd347195fc0fb1e71c11
Yash Gangrade <yashgangrade09@gmail.com>
2018-11-14 12:16:45 -0700

    A new php method

--- _includes/emailForm.php
+++ _includes/emailForm.php
@@ -72,7 +72,7 @@ if(isset($_POST['email'])) {
 $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
+mail($email_to, $email_subject, $email_message, $headers);  
 ?>
  
 <!-- include your own success html here -->
